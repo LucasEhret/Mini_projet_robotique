470aa5d8c900f1138e06cf0df11979fd566086c5
Theox <theo.pugin-bron@epfl.ch>
2021-04-17 21:34:02 +0200

    Déclaration d'une typedef enum pour les modes

--- ProjetEpuck/controle_thread.h
+++ ProjetEpuck/controle_thread.h
@@ -4,6 +4,14 @@
 #include "ch.h"
 #include "hal.h"
 
+//Modes disponibles
+typedef enum {
+	Mode0 =0,
+	Mode1,
+	Mode2,
+	Mode3,
+};
+
 void stop_thread(int mode_to_stop);
 void run_thread_manette(void);
 
